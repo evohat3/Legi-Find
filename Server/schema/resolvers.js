fc8087b383dc49fff8e35a255185a00b72eb1229
FuzzyGiraffee <121840200+FuzzyGiraffee@users.noreply.github.com>
2023-06-29 19:26:45 -0500

    added constants to resolvers.js

--- Server/schema/resolvers.js
+++ Server/schema/resolvers.js
@@ -1,7 +1,12 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Search } = require('../models');
+const { signToken } = require('../utils/auth');
+
 const resolvers = {
   Query: {
     hello: () => 'Hello, World!'
-  }
+  },
+  
 }
 
 module.exports = resolvers
