91af8467120f6d3389f60fc2b03d3aa4a3676439
FuzzyGiraffee <121840200+FuzzyGiraffee@users.noreply.github.com>
2023-06-29 19:40:11 -0500

    add and update user mutations

--- Server/schema/resolvers.js
+++ Server/schema/resolvers.js
@@ -7,6 +7,19 @@ const resolvers = {
     hello: () => 'Hello, World!'
   },
   Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    updateUser: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
