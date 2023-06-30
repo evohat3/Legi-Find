6ab4b6906e79ec663971a0c98cf9f116c9b0ca16
FuzzyGiraffee <121840200+FuzzyGiraffee@users.noreply.github.com>
2023-06-30 08:10:52 -0500

    fixed mutation server bug

--- Server/schema/typeDefs.js
+++ Server/schema/typeDefs.js
@@ -4,11 +4,14 @@ const typeDefs = gql`
   type Query {
     hello: String
   }
-
-
+  type User {
+    _id: ID
+    email: String
+  }
+  type Mutation {
+    addUser(email: String!, password: String!): User
+    updateUser(email: String, password: String): User
+    login(email: String!, password: String!): User
+  }
 `;
-  // type User {
-  // _id: ID
-  // email: String
-  // }
 module.exports = typeDefs
