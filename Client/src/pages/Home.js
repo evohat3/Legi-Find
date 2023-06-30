f99166ea2852b180bd5187dd6e23078ed8fc6e08
evohat3 <118646438+evohat3@users.noreply.github.com>
2023-06-29 21:18:17 -0500

    updates

---
+++ Client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import react from 'react'
+import { Typography } from 'material-ui/CircularProgress'
+
+export default function Home() {
+    return (
+        <Typography   
+        variant='h3'
+        color='primary'
+        align='center'>
+        Notes page Typography
+        </Typography>
+    )
+}
--- Server/models/User.js
+++ Server/models/User.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const { Schema } = mongoose;
-const bcrypt = require('bcrypt');
+const { Schema } = mongoose
+const bcrypt = require('bcrypt')
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true  
-    }
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 })
 
-module.exports = User;
+module.exports = User
