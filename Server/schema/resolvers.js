26fef84b443fbf3099fe12fbd1db42904449c2ff
FuzzyGiraffee <121840200+FuzzyGiraffee@users.noreply.github.com>
2023-06-29 19:34:18 -0500

    added login mutation

--- Server/schema/resolvers.js
+++ Server/schema/resolvers.js
@@ -6,7 +6,25 @@ const resolvers = {
   Query: {
     hello: () => 'Hello, World!'
   },
-  
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!User) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if(!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    }
+  }
 }
 
 module.exports = resolvers
