690e5d186055f37d7190b85d2892005bf1aaa5c2
FuzzyGiraffee <121840200+FuzzyGiraffee@users.noreply.github.com>
2023-07-03 20:16:32 -0500

    adding api call skeletons

--- Client/src/utils/API.js
+++ Client/src/utils/API.js
@@ -1,8 +1,39 @@
 //https://api.legiscan.com/?key=46f60bb5da729de39fe63400d12082db&op=getBill
+require('dotenv').config()
+const LEGI_KEY  = process.env.LEGI_KEY
 
+//call get bill from legiscan APIm fill in line 10 when we know exactly what we want form each bill
+export const getBill = (LEGI_KEY, bill_id) => {
+    return fetch(`https://api.legiscan.com/?key=${LEGI_KEY}&op=getBill${bill_id}`, {
+        method: 'POST',
+        body: JSON.stringify({
 
-export const getBill = (LEGI_KEY) => {
-    return fetch(`https://api.legiscan.com/?key=${LEGI_KEY}getBill`, {
+        }),
+        header: {
+            'Content-type': 'application/json',
+            }
+      });
+}
 
+//need to modify year variable to filter years
+export const getSearchState = (LEGI_KEY, state, query) => {
+    return fetch(`https://api.legiscan.com/?key=${LEGI_KEY}&op=getSearch&state=${state}&query=${query}` ,{
+        body: JSON.stringify({
+
+        }),
+        header: {
+            'Content-type': 'application/json',
+            }
+      });
+}
+
+export const getSearchSession = (LEGI_KEY, session_id, query) => {
+    return fetch(`https://api.legiscan.com/?key=${LEGI_KEY}&op=getSearch&id=${session_id}&query=${query}` ,{
+        body: JSON.stringify({
+
+        }),
+        header: {
+            'Content-type': 'application/json',
+            }
       });
 }
