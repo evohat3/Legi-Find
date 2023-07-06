7c6c0e13727c87d2c8ad5fdd73c4af09c1da59dc
evohat3 <118646438+evohat3@users.noreply.github.com>
2023-07-06 18:54:50 -0500

    update signup

--- Client/src/pages/Dashboard.js
+++ Client/src/pages/Dashboard.js
@@ -3,22 +3,11 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Auth from '../utils/auth';
-import { useLocation } from 'react-router-dom';
 
 export default function Dashboard(props) {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const token = queryParams.get('token');
-  const decodedToken = Auth.getProfile(token); // Pass the token to getProfile
-  if (token) {
-    Auth.login(token);
-  }
-  // Extract email from decoded token
-  const email = decodedToken ? decodedToken.email : '';
-  const first = decodedToken ? decodedToken.first : '';
-  const last = decodedToken ? decodedToken.last : '';
 
   const isLoggedIn = Auth.loggedIn();
+
   if (!isLoggedIn) {
     window.location.href = '/'; // Redirect to '/' if user is not logged in
     return null; // Render nothing if not logged in
@@ -34,8 +23,8 @@ export default function Dashboard(props) {
         }}
       >
         <Container sx={{ bgcolor: 'green' }}>
-          <Typography first={first} variant="h3" align="center">
-            Welcome {props.first} ({last})! You are logged in! with your email: ({email})
+          <Typography first={props.first} variant="h3" align="center">
+            Welcome {props.first} ({props.last})! You are logged in! with your email: ({props.email})
           </Typography>
           <Typography variant="h5" align="center"></Typography>
         </Container>
