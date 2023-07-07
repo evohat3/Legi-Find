524a08d1e824a49b5a8b9f94d2ce2ef017a94b9a
rnutall <rnutall1@gmail.com>
2023-07-06 19:39:02 -0500

    worked on footer

--- Client/src/components/Footer.js
+++ Client/src/components/Footer.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import Container from '@mui/material/Container'
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton';
 import linkedIn from "./assets/LI-In-Bug.png";
 import "../index.css";
 
@@ -9,11 +11,21 @@ const Footer = () => {
   return (
     <footer backgroundcolor="primary">
       <Container maxWidth="xl" sx={{backgroundColor: "skyblue"}}>
-        <Typography variant="h6" align="center">
+        <Typography>
+        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none'} }}>
+        <IconButton
+          size="large"
+          margin= "center"
+          aria-haspopup="true"
+          onClick={linkedIn}
+          color="inherit"
+        variant="h6" align="center">
+          </IconButton>
+          </Box>
           This project was created by.
           <br></br>
           <button><a href="https://www.linkedin.com/in/rodney-nutall?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2FYnmq%2FjJTg2DHKUbE0eWKw%3D%3D" className="icon">
-        <img  src={linkedIn} width={30} height={30} margin={60} spaceBetween={30} />
+        <img  src={linkedIn} width={30} height={30}  />
       </a>
       <br></br>
       Rodney Nutall
