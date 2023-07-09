ed23a7e6e7dd41c0c359f165b5318bf294c3d659
evohat3 <118646438+evohat3@users.noreply.github.com>
2023-07-09 16:40:51 -0500

    comment out used imports

--- Client/src/components/SearchPageLite.js
+++ Client/src/components/SearchPageLite.js
@@ -7,7 +7,7 @@ import Grid from '@mui/material/Grid'
 import Button from '@mui/material/Button'
 // import { getSearchState, getSearchSession} from '../utils/API'
 import SelectSearchTypeDropdown from '../components/SelectSearchTypeDropdown'
-import { borderColor, height, textAlign, width } from '@mui/system'
+// import { borderColor, height, textAlign, width } from '@mui/system'
 import { Typography } from '@mui/material'
 
 export default function Search() {
